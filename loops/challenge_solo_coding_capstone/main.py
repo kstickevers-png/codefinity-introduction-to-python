aa4b8052790bb9957de538566a41edafead4ea7a
kstickevers-png <>
2025-12-23 14:37:08 +0000

    Challenge: Solo Coding Capstone

--- loops/challenge_solo_coding_capstone/main.py
+++ loops/challenge_solo_coding_capstone/main.py
@@ -4,3 +4,13 @@ inventory = {
     "Eggs": [225, 2.12, 1.99],  # Eggs should be sold at a discount
     "Apples": [9, 1.50, 1.35]   # Apples need to be restocked
 }
+
+for item, values in inventory.items():
+    stock,price,discount_price = values
+    if stock < 30:
+        print(f"{item} need restocking.")
+    elif stock > 100:
+        print(f"{item} should be sold at the discounted price of {discount_price}.")
+    else:
+        print(f"{item} should be sold at the regular price of {price}.")
+    
