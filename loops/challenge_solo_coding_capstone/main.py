2d5df4a7b38549c117f1df0ffa149c792f04a9b8
kstickevers-png <>
2025-12-23 14:23:25 +0000

    Challenge: Solo Coding Capstone

---
+++ loops/challenge_solo_coding_capstone/main.py
@@ -0,0 +1,6 @@
+# Inventory dictionary with stock, price, and discount price
+inventory = {
+    "Bread": [42, 1.20, 0.99],  # "Item": [current stock, regular price, discounted price]
+    "Eggs": [225, 2.12, 1.99],  # Eggs should be sold at a discount
+    "Apples": [9, 1.50, 1.35]   # Apples need to be restocked
+}
